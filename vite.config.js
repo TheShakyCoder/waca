70a10d33f41965f53fddcf629bfed99f5d9e4bf9
Sharif Khan <sharif.khan@mac.com>
2026-08-06 13:26:31 +0100

    ddev working

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import fs from 'fs';
+import tailwindcss from '@tailwindcss/vite';
 
-const port = 5173
-const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`
+const port = 5173;
 
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
+        tailwindcss(),
         vue({
             template: {
                 transformAssetUrls: {
@@ -22,15 +21,17 @@ export default defineConfig({
             },
         }),
     ],
-    server: {
-        cors: true,
-        host: "0.0.0.0",
-        port: port,
-        strictPort: true,
-        origin: origin,
-        hmr: {
-            host: process.env.DDEV_HOSTNAME,
-            protocol: 'wss'
+    server: process.env.DDEV_PRIMARY_URL
+        ? {
+            cors: true,
+            host: '0.0.0.0',
+            port: port,
+            strictPort: true,
+            origin: `${process.env.DDEV_PRIMARY_URL}:${port}`,
+            hmr: {
+                host: process.env.DDEV_HOSTNAME,
+                protocol: 'wss',
+            },
         }
-    },
+        : {},
 });
